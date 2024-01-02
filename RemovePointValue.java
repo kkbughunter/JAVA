9e8756ff9987cfee7093e4dd570571c370a74459
Karthikeyan <karthikeyan.aas@gmail.com>
2024-01-02 20:02:32 +0530

    java program

---
+++ RemovePointValue.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class RemovePointValue{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float data = sc.nextFloat();
+        System.out.println((int)data);
+    }
+}
---
+++ RoundOfTheValue.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class RoundOfTheValue {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float data = sc.nextFloat();
+        System.out.println(Math.round(data));
+    }
+}
